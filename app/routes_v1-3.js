5d9bcdf68766f9cdb92359e4cd0753a75153e064
David Thomas <david.thomas@environment-agency.gov.uk>
2022-07-29 12:19:05 +0100

    Delete dead code, tidy file structure, rename files, fix url structure

---
+++ app/routes_v1-3.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const { get } = require('browser-sync')
+const router = express.Router()
+const fs = require('fs')
+
+
+
+var folder = "v1-3/_find-adviser/filters"
+var versionServiceName = "Find an adviser"
+router.use(function (req, res, next) {
+  // store in locals this can then be used in pages as {{folder}} etc
+  res.locals.folder=folder
+  res.locals.versionServiceName=versionServiceName
+  next()
+})
+
+
+var searchColumn = 'DEF_SearchTextAll'
+
+// Routes
+router.get('/_find-adviser/search', function(req, res) {
+    res.render(folder + '/filter-page', { results: filterRegister(req.query.name, req.query.registers, req.query.statuses, req.query.country, req.query.category), url: req.url  })
+})
+
+router.get('/_find-adviser/adviser-details/:adviserNumber', function(req, res) {
+    res.render(folder + '/adviser-details', findAdviser(req.params.adviserNumber))
+})
+
+
+// Functions
+function findAdviser(adviserNumber) {
+    return getRegisterData('register').find(element => element.EA_FileNumber === adviserNumber)
+}
+
+function filterRegister(name, registers, statuses, country, category) {
+    name = name.toLowerCase()
+
+    let registerData = getRegisterData('register')
+
+    if (name) {
+        registerData = registerData.filter(element => element[searchColumn].includes(name))
+    }
+
+    if (registers != '_unchecked') {
+        registerData = registerData.filter(element => registers.includes(element.DEF_Register))
+    }
+
+    if (statuses != '_unchecked') {
+        registerData = registerData.filter(element => statuses.includes(element.EA_Status))
+    }
+
+    if (country) {
+        registerData = registerData.filter(element => element.EA_Country === country)
+    }
+
+    if (category) {
+        registerData = registerData.filter(element => element.EA_ProductCategory === category)
+    }
+
+    return registerData
+}
+
+function getRegisterData(registerName) {
+    return require('./views/' + folder + '/data/registers/register.json')
+}
+
+module.exports = router
