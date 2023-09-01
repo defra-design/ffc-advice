93e7fe21276f552e0809b4471dcd9718e5b11e95
Capcassierobinson <crobinsoncap@gmail.com>
2023-09-01 12:09:47 +0100

    Updates for Sprint 4 testing
    Added card concept
    Added updated AI concept
    Added updated filter concept
    Updated navigation changes

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,13 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/Version_4/Filter/filterfirst1', function(request, response) {
+
+    var decision = request.session.data['decision']
+    if (decision == "Yes"){
+        response.redirect('/Version_4/Filter/option2filter')
+    } else {
+        response.redirect('/Version_4/Filter/filterfirst2')
+    }
+})
