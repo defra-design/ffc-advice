659a1ce64eba671d3fed9f40505bbe11710230d2
emma-gray <90691992+emma-gray@users.noreply.github.com>
2023-10-19 14:20:24 +0100

    testing

--- app/routes.js
+++ app/routes.js
@@ -3,29 +3,28 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
-// Add your routes here
+// Version 4 routes
 
-router.post('/Version_4/Filter/filterfirst1', function(request, response) {
-
-    var decision = request.session.data['decision']
-    if (decision == "Yes"){
-        response.redirect('/Version_4/Filter/option2filter')
-    } else {
-        response.redirect('/Version_4/Filter/filterfirst2')
-    }
-})
+router.post("/Version_4/Filter/filterfirst1", function (request, response) {
+  var decision = request.session.data["decision"];
+  if (decision == "Yes") {
+    response.redirect("/Version_4/Filter/option2filter");
+  } else {
+    response.redirect("/Version_4/Filter/filterfirst2");
+  }
+});
 
 
 // Version 5 routes
 
 router.post("/Version_5/Filter/filterfirst1", function (request, response) {
-    var choice = request.session.data["choice"];
-    if (choice == "Yes") {
-      response.redirect("/Version_5/Filter/option3filter");
-    } else {
-      response.redirect("/Version_5/Filter/filter3");
-    }
-  });
+  var choice = request.session.data["choice"];
+  if (choice == "Yes") {
+    response.redirect("/Version_5/Filter/option3filter");
+  } else {
+    response.redirect("/Version_5/Filter/filter3");
+  }
+});
