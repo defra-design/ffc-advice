039e0293437df655d53515bbebba5c33d89da637
Emma Gray <90691992+emma-gray@users.noreply.github.com>
2023-11-13 14:57:58 +0000

    update of broken links
    update of broken links

--- app/routes.js
+++ app/routes.js
@@ -6,26 +6,42 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
 // Add your routes here
 
-router.post('/Version_4/Filter/filterfirst1', function(request, response) {
+router.post('/Alpha/Version_4/Filter/filterfirst1', function(request, response) {
 
     var decision = request.session.data['decision']
     if (decision == "Yes"){
-        response.redirect('/Version_4/Filter/option2filter')
+        response.redirect('/Alpha/Version_4/Filter/filterfirst1')
     } else {
-        response.redirect('/Version_4/Filter/filterfirst2')
+        response.redirect('/Alpha/Version_4/Filter/filterfirst2')
     }
 })
 
 
 // Version 5 routes
 
-router.post("/Version_5/Filter/filterfirst1", function (request, response) {
+router.post('/Alpha/Version_5/Filter/filterfirst1', function (request, response) {
     var choice = request.session.data["choice"];
     if (choice == "Yes") {
-      response.redirect("/Version_5/Filter/option3filter");
+      response.redirect("/Alpha/Version_5/Filter/option3filter");
     } else {
-      response.redirect("/Version_5/Filter/filter3");
+      response.redirect("/Alpha/Version_5/Filter/filter3");
     }
-  });
+  })
+
+
+ //Beta routes
+
+ //SP ++
+ router.post("/Beta/SP-plus/Filter/filterfirst1", function (request, response) {
+  var choice = request.session.data["choice"];
+  if (choice == "Yes") {
+    response.redirect("/Beta/SP-plus/Filter/option3filter");
+  } else {
+    response.redirect("/Beta/SP-plus/Filter/filter3");
+  }
+});
+ 
+  
