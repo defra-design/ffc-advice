634ab81b5d151022af726e253c5c2b8eda461d31
Casto227 <alessandro.castorino@capgemini.com>
2025-06-19 16:21:28 +0100

    Updated based on UR sessions

--- app/routes.js
+++ app/routes.js
@@ -9,33 +9,33 @@ const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
 
-router.post('/Alpha/Version_4/Filter/filterfirst1', function(request, response) {
+router.post('/Alpha/Version_4/Filter/filterfirst1', function (request, response) {
 
-    var decision = request.session.data['decision']
-    if (decision == "Yes"){
-        response.redirect('/Alpha/Version_4/Filter/filterfirst1')
-    } else {
-        response.redirect('/Alpha/Version_4/Filter/filterfirst2')
-    }
+  var decision = request.session.data['decision']
+  if (decision == "Yes") {
+    response.redirect('/Alpha/Version_4/Filter/filterfirst1')
+  } else {
+    response.redirect('/Alpha/Version_4/Filter/filterfirst2')
+  }
 })
 
 
 // Version 5 routes
 
 router.post('/Alpha/Version_5/Filter/filterfirst1', function (request, response) {
-    var choice = request.session.data["choice"];
-    if (choice == "Yes") {
-      response.redirect("/Alpha/Version_5/Filter/option3filter");
-    } else {
-      response.redirect("/Alpha/Version_5/Filter/filter3");
-    }
-  })
+  var choice = request.session.data["choice"];
+  if (choice == "Yes") {
+    response.redirect("/Alpha/Version_5/Filter/option3filter");
+  } else {
+    response.redirect("/Alpha/Version_5/Filter/filter3");
+  }
+})
 
 
- //Beta routes
+//Beta routes
 
- //SP ++
- router.post("/Beta/SP-plus/Filter/filterfirst1", function (request, response) {
+//SP ++
+router.post("/Beta/SP-plus/Filter/filterfirst1", function (request, response) {
   var choice = request.session.data["choice"];
   if (choice == "Yes") {
     response.redirect("/Beta/SP-plus/Filter/option3filter");
@@ -43,5 +43,11 @@ router.post('/Alpha/Version_5/Filter/filterfirst1', function (request, response)
     response.redirect("/Beta/SP-plus/Filter/filter3");
   }
 });
- 
-  
+
+
+
+// Route to reset session data
+router.get('/reset', function (req, res) {
+  req.session.data = {};
+  res.redirect('/');
+});
